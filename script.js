ce0f8f410b1542b252652f50fc564ed91fba554f
alamrumambi <alam.abumaryam@gmail.com>
2020-03-25 23:46:50 +0700

    app version 1 (finish)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var result = 0;
+var temp = [];
 function scr(input){
     var hom = document.getElementById("opening");
     var welcome = document.getElementById("pilih-angka");
@@ -9,11 +10,20 @@ function scr(input){
     var layer5 = document.getElementById("layer5");
     var layer6 = document.getElementById("layer6");
     var layer7 = document.getElementById("layer7");
+    var result1 = document.getElementById("result");
+    var wrongResult = document.getElementById("wrongResult");
+    var finish = document.getElementById("finish");
+    var hasil = document.getElementById("ending");
 
     if (input == 'next'){
         if (welcome.style.display == "block"){
             welcome.style.display = "none";
             layer1.style.display = "block";
+        }
+        else if (result1.style.display == "block"){
+            result1.style.display = "none";
+            hasil.innerHTML = result;
+            finish.style.display = "block";
         }
         else {
             welcome.style.display = "block";
@@ -23,7 +33,7 @@ function scr(input){
 
     else if (input == 'home'){
         var arr = [welcome, layer1, layer2, layer3, layer4,
-            layer5, layer6, layer7]
+            layer5, layer6, layer7, wrongResult, finish]
         
         for (let i in arr){
             if (arr[i].style.display == "block"){
@@ -36,18 +46,25 @@ function scr(input){
     }
 
     else if (input == 'back'){
-        var arr = [layer1, layer2, layer3, layer4,
-            layer5, layer6, layer7]
+        var arr = [[layer1, 1], [layer2, 2], [layer3, 4], [layer4, 8],
+        [layer5, 16], [layer6, 32], [layer7, 64]];
         
         for (let i in arr){
-            if (arr[i].style.display == "block"){
-                arr[i].style.display = "none";
+            if (arr[i][0].style.display == "block"){
+                arr[i][0].style.display = "none";
                 if (i == "0") {
                     welcome.style.display = "block";
                     break;
                 }
                 else {
-                    arr[Number(i) - 1].style.display = "block";                        
+                    arr[Number(i) - 1][0].style.display = "block";
+                    for (let j in temp){
+                        if (temp[j] == Number(i) - 1){
+                            result -= arr[Number(i) - 1][1];
+                            temp.pop();
+                            break;
+                        }
+                    }                     
                     break;
                 }
             }
@@ -60,8 +77,20 @@ function scr(input){
 
         for (let i in arr){
             if (arr[i][0].style.display == "block"){
-                if (input == 'layerY') result += arr[i][1];
-                if (Number(i) == arr.length - 1) alert(result);
+                if (input == 'layerY'){
+                    result += arr[i][1];
+                    temp.push(Number(i));
+                } 
+                if (Number(i) == arr.length - 1){
+                    arr[i][0].style.display = "none";
+                    if (result > 100 || result < 1){
+                        wrongResult.style.display = "block";
+                    }
+                    else {
+                        result1.style.display = "block";
+                    }
+                    break;
+                }
                 arr[i][0].style.display = "none";
                 arr[Number(i) + 1][0].style.display = "block";
                 break;
